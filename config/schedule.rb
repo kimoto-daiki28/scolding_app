c1b9e17cede087b9966a6ebcd90e64afa00c8239
github heizoudaiki <daiki.kimoto0702@gmail.com>
2021-06-22 16:18:04 +0900

    lib/tasks/push.rakeファイルを作成

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,20 +1,11 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+require File.expand_path(File.dirname(__FILE__) + "/environment")
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
+rails_env = ENV['RAILS_ENV'] || :development
 
-# Learn more: http://github.com/javan/whenever
+set :environment, rails_env
+set :output, "#{Rails.root}/log/cron.log"
+
+# every 1.day, at: ['9:00 pm'] do # タスクを処理するペースを記載する。（毎21時に実行）
+every 5.minites
+  rake 'push_line:push_line_message_morning'
+end
---
+++ lib/tasks/push.rake
@@ -0,0 +1,15 @@
+namespace :push_line do 
+  desc "push_line"
+  task push_line_message_evening: :environment do # 以下にpush機能のタスクを書く。
+    message = {
+      type: 'text',
+      text: '本日は何か無駄遣いをしましたか？した場合は「した」、してない場合は「してない」と入力してください。'
+    }
+    client = Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+    response = client.push_message(ENV["LINE_CHANNEL_USER_ID"], message)
+    p response
+  end
+end
