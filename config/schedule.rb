bc17a6cd7ab6f86c5cb7c0ec224dae2ecaf877c6
github heizoudaiki <daiki.kimoto0702@gmail.com>
2021-06-23 13:25:44 +0900

    [Fix]config/schedule.rbを修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,6 +6,6 @@ set :environment, rails_env
 set :output, "#{Rails.root}/log/cron.log"
 
 # every 1.day, at: ['9:00 pm'] do # タスクを処理するペースを記載する。（毎21時に実行）
-every 5.minites
-  rake 'push_line:push_line_message_morning'
+every 10.minutes do
+  rake 'push_line:push_line_message_evening'
 end
