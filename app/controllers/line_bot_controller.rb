84c888d873b780ebe61a9bc2f8fa39fb809154f8
github heizoudaiki <daiki.kimoto0702@gmail.com>
2021-06-30 11:20:12 +0900

    [Modify]line_bot_controllerを編集
    quickReplyが完成

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -10,26 +10,7 @@ class LineBotController < ApplicationController
     end
     events = client.parse_events_from(body)
     events.each do |event|
-      # case event 
-      # when Line::Bot::Event::Message
       @message = event.message['text']
-      # if ['した', 'してない'].include?(event.message['text'])
-      #   case event.message['text']
-      #   when 'した'
-      #     message = ::LineClient.fiest_quick_reply
-      #     client.reply_message(event['replyToken'], message)
-      #     # response = '何を無駄遣いしましたか？'
-      #   when 'してない'
-      #     response = "えらい！よく我慢できました！\nあなたのそのブレない心に称賛を送ります！\nその調子で誘惑に打ち勝っていきましょう！！"
-      #   end
-      # elsif ['お菓子', 'お酒', 'ネットショッピング', 'ジュース'].include?(event.message['text'])
-      #   response = 'いくらですか？'
-      # elsif ('1'..'30000').include?(event.message['text'])
-      #   response = "#{event.message['text']}円も使ったんですか？バカですか？"
-      # else
-      #   response = '認識できませんでした。もう一度入力してください。'
-      # end
-
       case event
       when Line::Bot::Event::Message
         case event.type
@@ -37,32 +18,32 @@ class LineBotController < ApplicationController
           case @message
           when 'した'
             message = ::LineClient.second_quick_reply
-            client.reply_message(event['replyToken'], message)
           when 'してない'
             response = "えらい！よく我慢できました！\nあなたのそのブレない心に称賛を送ります！\nその調子で誘惑に打ち勝っていきましょう！！"
             message = {
               type: 'text',
               text: response
             }
-            client.reply_message(event['replyToken'], message)
-          when ['お菓子', 'お酒', 'ネットショッピング', 'ジュース'].include?(@message)
+          when 'お菓子', 'お酒', 'ネットショッピング', 'ジュース'
             response = "いくらでしたか？"
             message = {
               type: 'text',
               text: response
             }
-            client.reply_message(event['replyToken'], message)
-          when ('1'..'30000').include?(@message)
+          when ('1'..'30000')
             response = "#{@message}円も使ったんですか？バカですか？"
             message = {
               type: 'text',
               text: response
             }
           else
-            response = "いくらでしたか？"
-            
-            client.reply_message(event['replyToken'], message)
+            response = '認識できませんでした。もう一度入力してください。'
+            message = {
+              type: 'text',
+              text: response
+            }
           end
+          client.reply_message(event['replyToken'], message)
         end
       end
     end
@@ -77,11 +58,4 @@ class LineBotController < ApplicationController
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
   end
-
-  # def message_type(response)
-  #   message = {
-  #     type: 'text',
-  #     text: response
-  #   }
-  # end
 end
