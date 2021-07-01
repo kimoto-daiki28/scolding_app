a5cd5676b0696f5eebfc37dfc8a9fdc8f8217ed2
github heizoudaiki <daiki.kimoto0702@gmail.com>
2021-07-01 17:45:45 +0900

    [Add]user.rbにline_loginメソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,42 @@
 class User < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :line_id, presence: true, uniqueness: true
+
+  def self.line_login(code)
+    uri = URI.parse('https://api.line.me/oauth2/v2.1/token')
+    req = Net::HTTP::Post.new(uri)
+    req.content_type = 'application/x-www-form-urlencoded'
+    req.set_form_data(
+      'client_id' => ENV['CLIENT_ID'],
+      'client_secret' => ENV['CLIENT_SECRET'],
+      'code' => code,
+      'grant_type' => 'authorization_code',
+      'redirect_uri' => ENV['SITE_URL']
+    )
+    req_options = {
+      use_ssl: uri.scheme == 'https'
+    }
+
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(req)
+    end
+
+    result = JSON.parse(response.body)
+    token = result['access_token']
+    uri_2 = URI.parse('https://api.line.me/v2/profile')
+    req_2 = Net::HTTP::Get.new(uri_2)
+    req_2['authorization'] = "Bearer #{token}"
+    req_options_2 = {
+      use_ssl: uri_2.scheme == 'https'
+    }
+    response = Net::HTTP.start(uri_2.hostname, uri_2.port, req_options_2) do |http|
+      http.request(req_2)
+    end
+
+    result = JSON.parse(response.body)
+    user_id = result['userId']
+    user_name = result['displayName']
+    user = User.find_or_create_by(name: user_name, line_id: user_id)
+    user.id
+  end
 end
