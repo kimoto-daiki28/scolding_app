c0e0dc7abaa8db0d444bab1d9f6997ff94df6c2c
github heizoudaiki <daiki.kimoto0702@gmail.com>
2021-07-09 13:05:27 +0900

    [Add]ログアウト機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,15 @@ class UsersController < ApplicationController
   skip_before_action :login_require, only: :new
   def new
     session[:user_id] = User.line_login(params[:code])
-    redirect_to root_path, notice: 'ログインしました'
+    redirect_to root_path, notice: 'ログインしました。'
+  end
+
+  def destroy
+    @current_user&.destroy
+    redirect_to root_path, notice: 'ログアウトしました。'
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'tops#index'
-  resources :users, only: %i[new]
+  resources :users, only: %i[new destroy show]
   resources :wastings, only: %i[create]
   resources :terms_of_services, only: %i[index]
   resources :privacy_policys, only: %i[index]
