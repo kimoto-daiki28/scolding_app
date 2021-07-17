b90dfd459ad01d3a5d966c8c87b0503855773628
github heizoudaiki <daiki.kimoto0702@gmail.com>
2021-07-17 22:18:37 +0900

    [Fix]line_message_api_spec.rbを修正

--- spec/requests/line_message_api_spec.rb
+++ spec/requests/line_message_api_spec.rb
@@ -3,9 +3,8 @@ require 'rails_helper'
 RSpec.describe 'Message_api', type: :request do
   describe 'POST /wastings' do
     it 'Completed 200 OK ' do
-      create(:user)
-      post wastings_path, params: { message: { text: 'した' } }
-      expect(response).to have_http_status(200)
+      post '/wastings', params: { message: { text: 'した' } }
+      expect(response).to have_http_status(:ok)
     end
   end
 end
